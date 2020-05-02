4e2bf7079476a4dcc52a938378a4cd0f728e9ada
Joaquin Fernandez <copako12@gmail.com>
2020-05-02 00:50:25 -0300

    Updates

---
+++ precache-manifest.3f3676fc2db57020a35d52247ab0d7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6612d135fec2759450275415c6daac",
+    "url": "/the-box/index.html"
+  },
+  {
+    "revision": "fb889be1a192c3125e66",
+    "url": "/the-box/static/css/main.f82aa52e.chunk.css"
+  },
+  {
+    "revision": "8819dbdd1b9b64794df0",
+    "url": "/the-box/static/js/2.dc9d025d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-box/static/js/2.dc9d025d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb889be1a192c3125e66",
+    "url": "/the-box/static/js/main.f401d298.chunk.js"
+  },
+  {
+    "revision": "51371047a3e3677de062",
+    "url": "/the-box/static/js/runtime-main.4da536a8.js"
+  }
+]);
